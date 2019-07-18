7f682ab4ae51fc2cef60814c6add8684a032b5cc
Francesco Mecca <me@francescomecca.eu>
2019-07-18 13:15:03 +0200

    fragments2

--- app/src/main/java/com/apollon/ApollonFragment.kt
+++ app/src/main/java/com/apollon/ApollonFragment.kt
@@ -1,21 +1,27 @@
 package com.apollon
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class ApollonFragment : Fragment(){
+
+    lateinit var mView: View
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment, container, false)
+        mView = inflater.inflate(R.layout.fragment, container, false)
+        return mView
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    fun updateTextView() {
+        mView.findViewById<TextView>(R.id.text1)?.text = "prova"
     }
 
 
--- app/src/main/java/com/apollon/MainActivity.kt
+++ app/src/main/java/com/apollon/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.apollon
 
+import android.media.AudioManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.ButtonBarLayout
 
 class MainActivity : AppCompatActivity() {
+
+    val fm = supportFragmentManager
+
+    lateinit var button : Button
+
+    var i = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        volumeControlStream = AudioManager.STREAM_MUSIC
+
+        button = findViewById(R.id.button2)
+        button.setOnClickListener { v: View ->
+
+            val transaction = fm.beginTransaction()
+            val frag = ApollonFragment()
+            transaction.replace(R.id.main, frag)
+            transaction.commit()
+            fm.executePendingTransactions()
+            frag.updateTextView()
+        }
+
     }
 }

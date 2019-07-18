fda53a9fc5eb23b3137d0130130d13f895a3c41e
Francesco Mecca <me@francescomecca.eu>
2019-07-18 11:42:40 +0200

    fragment

---
+++ app/src/main/java/com/apollon/ApollonFragment.kt
@@ -0,0 +1,22 @@
+package com.apollon
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ApollonFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+
+}
